4249b32f6c237fe3e90bc674f2222a7498ff5603
Aziz <azizsaboor@gmail.com>
2016-07-25 16:43:58 +0800

    on start app init from redis

--- countwitt.js
+++ countwitt.js
@@ -4,10 +4,10 @@ var ntwitter = require('ntwitter'),
     redis = require('redis'),
     redisClient = redis.createClient();
 
-    // please note that './' requests node to look at the current dir
-    // without it, the 'node_modules' will be searched instead
-    // initialise twitter object to allow streaming
-    credentials = require('./credentials.json'),
+// please note that './' requests node to look at the current dir
+// without it, the 'node_modules' will be searched instead
+// initialise twitter object to allow streaming
+credentials = require('./credentials.json'),
     twitter = ntwitter(credentials),
 
 
@@ -19,39 +19,52 @@ var ntwitter = require('ntwitter'),
         var trackedWords = words,
             counts = {};
 
-        // please note that in js ...
-        // 'counts.awesome = 0' can also be accessed with
-        // 'counts[awesome] = 0'. It's generally recommended to ...
-        // ... stick to the dot notation format. However, in this particular circumstance
-        // it is advisable to take advantage of latter method to allow for other than hard coding!
+        // iterate through words, 
+        // and initialise each by retrieving the existing count from redis
         trackedWords.forEach(function (word) {
-            counts[word] = 0;
-            redisClient.set(word, 0);
+            // counts[word] = 0;
+            // redisClient.set(word, 0);
+
+            redisClient.get(word, function (err, wordCount) {
+
+                // check for error
+                if (err !== null) {
+                    console.log('[ERROR] ' + err);
+
+                    // stop application
+                    return;
+                }
+
+                // please note that 'wordCount' will be null if key is missing
+                // so the following initialises the counter with the retrieved data, or 0
+                counts[word] = parseInt(wordCount, 10) || 0;
+
+                // define the stream
+                // param 1 - type of stream
+                // param 2 - filter rules (i.e. by words, by location etc)
+                // param 3 - callback for whenever relevant tweet comes through
+                twitter.stream(
+                    'statuses/filter',
+
+                    { 'track': trackedWords },
+
+                    function (stream) {
+                        stream.on('data', function (tweet) {
+                            trackedWords.forEach(function (word) {
+                                if (tweet.text.indexOf(word) > -1) {
+                                    counts[word] += 1;
+
+                                    // increment the count on redis
+                                    redisClient.incr(word);
+                                }
+                            });
+                        });
+                    }
+                );
+
+            });
+
         });
-
-        // define the stream
-        // param 1 - type of stream
-        // param 2 - filter rules (i.e. by words, by location etc)
-        // param 3 - callback for whenever relevant tweet comes through
-        twitter.stream(
-            'statuses/filter',
-
-            { 'track': trackedWords },
-
-            function (stream) {
-                stream.on('data', function (tweet) {
-                    trackedWords.forEach(function (word) {
-                        if (tweet.text.indexOf(word) > -1) {
-                            counts[word] += 1;
-
-                            // increment the count on redis
-                            redisClient.incr(word);
-                        }
-                    });
-                });
-            }
-        );
-
 
         return counts;
     };
